cd9f8c5dd834a691c9345c0e436ec9c5505200a9
maseokdoo <madongseok0303@gmail.com>
2023-12-04 23:47:05 +0900

    finish to make basic game function

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # first-trial-of-makeing-javascript-game---the-game-is-google-s-dinosaur-game
 this project is my first game project ( to learn how to make game)
 This project will be first to be best game developer
+
+This file will be the note to record what I didn't know
+
+
--- main.js
+++ main.js
@@ -30,23 +30,61 @@ class Cactus{
 
 var timer = 0;
 var plenty_of_cactus = [];
-
+var jumptimer = 0;
+var jumping = false;
+var animation;
 
 function Func_execute_for_every_Frame(){
-    requestAnimationFrame(Func_execute_for_every_Frame)
+    animation = requestAnimationFrame(Func_execute_for_every_Frame);
     timer++;
     ctx.clearRect(0,0, canvas.width, canvas.height);
 
-    if(timer % 120 === 0){
+    if(timer % 200 === 0){
         var cactus = new Cactus();
         plenty_of_cactus.push(cactus);
     }
-    plenty_of_cactus.forEach((a)=>{
+    plenty_of_cactus.forEach((a, i, o)=>{
+        //좌표가 0 미만이면 제거하라는 코드
+        if (a.x < 0){
+            o.splice(i, 1)
+        }
         a.x--;
+        crash_check(dino, a);
         a.draw();
     })
+
+    if(jumping == true){
+        dino.y--;
+        jumptimer++;
+    }
+    if(jumping == false){
+        if(dino.y < 200){
+            dino.y++;
+        }
+    }
+    if(jumptimer>100){
+        jumping = false;
+        jumptimer = 0;
+    }
     
     dino.draw()
 }
 
+//충돌확인
+function crash_check(dino, cactus){
+    var x_difference = cactus.x - (dino.x + dino.width);
+    var y_difference = cactus.y - (dino.y + dino.height);
+    if( x_difference < 0 && y_difference < 0){
+        ctx.clearRect(0,0, canvas.width, canvas.height);
+        cancelAnimationFrame(animation)
+    }
+}
+
+
 Func_execute_for_every_Frame();
+
+document.addEventListener('keydown', function(e){
+    if (e.code === 'Space'){
+        jumping = true;
+    }
+})
